cd9a6bb60875074b1aa05d019b05ab8ac392aae8
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-06-18 15:50:05 +0530

    Update routes

--- app/api/chatgpt/route.ts
+++ app/api/chatgpt/route.ts
@@ -1,29 +1,29 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 export const POST = async (request: Request) => {
   const { question } = await request.json();
 
   try {
-    const response = await fetch('https://api.openai.com/v1/chat/completions', {
-      method: 'POST',
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
       },
       body: JSON.stringify({
-        model: 'gpt-3.5-turbo',
+        model: "gpt-3.5-turbo",
         messages: [
           {
-            role: 'system',
+            role: "system",
             content:
-              'Your are a knowledgable assistant that provides quality informations'
+              "Your are a knowledgable assistant that provides quality informations",
           },
           {
-            role: 'user',
-            content: `Tell me ${question}`
-          }
-        ]
-      })
+            role: "user",
+            content: `Tell me ${question}`,
+          },
+        ],
+      }),
     });
     const responseData = await response.json();
     const reply = responseData.choices[0].message.content;
@@ -31,7 +31,7 @@ export const POST = async (request: Request) => {
     return NextResponse.json({ reply });
   } catch (error: any) {
     return NextResponse.json({
-      error: error.message
+      error: error.message,
     });
   }
 };
