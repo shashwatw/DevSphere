61dbc31b627f5321ef10a32f87f290c3ac604e36
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2024-06-23 13:46:45 +0530

    Minor changes

--- app/api/chatgpt/route.ts
+++ app/api/chatgpt/route.ts
@@ -1,14 +1,15 @@
 import { NextResponse } from "next/server";
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const genAI = new GoogleGenerativeAI("AIzaSyB-V08FJ0ojP5WqgJUHZMzbEKJreIvO4sc");
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 export const POST = async (request: Request) => {
   const { question } = await request.json();
 
   try {
-    const prompt = `Tell me ${question} according to TINYMCE editor and enclose code inside <pre><code></code></pre>`;
+    const prompt = `Please provide a comprehensive explanation of ${question} with beautiful formatting enclosing code inside <pre><code></code></pre>`;
+
     const result = await model.generateContent(prompt);
     const response = await result.response;
     const text = await response.text();
